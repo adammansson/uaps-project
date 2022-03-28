50c3db22bec16bf356d69e4b358a73902428078c
Adam Månsson <adam.s.mansson@gmail.com>
2022-03-28 15:54:24 +0200

    Added insertionsort

---
+++ insertionsort/src/pfk/InsertionSorter.java
@@ -0,0 +1,16 @@
+package pfk;
+
+public class InsertionSorter {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
